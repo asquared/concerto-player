71a6907e066c8b0c29802f189cf209538d5cc26a
Andrew Armenia <armena@andrewsdesktop.inside.asquaredlabs.com>
2009-07-11 14:28:23 -0400

    fixed finish_setup.sh to include firefox dependencies

--- base-system/finish_setup.sh
+++ base-system/finish_setup.sh
@@ -4,6 +4,10 @@ export LC_ALL=C
 mount -t proc proc /proc
 apt-get -y update
 apt-get -y upgrade
+# basic system tools
 apt-get -y install alsa-tools alsa-utils ethtool lm-sensors mpg123 openssh-server wireless-tools wpasupplicant xserver-xorg xbase-clients tightvncserver wget vim
+# stuff to make firefox work right
+apt-get -y install libgtk2.0-0 xfwm4
+
 umount /proc
 # rm -rf files_we_dont_need
