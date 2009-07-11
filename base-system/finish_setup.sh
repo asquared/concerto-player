4615925284107dc6966721b71f81223cd4f44421
Andrew Armenia <armena@andrewsdesktop.inside.asquaredlabs.com>
2009-07-11 12:47:05 -0400

    base system: added some extra packages
    added tightvncserver (for testing), xbase-clients, vim, and wget

--- base-system/finish_setup.sh
+++ base-system/finish_setup.sh
@@ -4,6 +4,6 @@ export LC_ALL=C
 mount -t proc proc /proc
 apt-get -y update
 apt-get -y upgrade
-apt-get -y install alsa-tools alsa-utils ethtool lm-sensors mpg123 openssh-server wireless-tools wpasupplicant xserver-xorg
+apt-get -y install alsa-tools alsa-utils ethtool lm-sensors mpg123 openssh-server wireless-tools wpasupplicant xserver-xorg xbase-clients tightvncserver wget vim
 umount /proc
 # rm -rf files_we_dont_need
