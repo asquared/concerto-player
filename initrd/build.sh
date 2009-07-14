2e30ff8fc85040e4e3347b3296a38be81257d4ad
Andrew Armenia <armena@andrewsdesktop.inside.asquaredlabs.com>
2009-07-14 18:08:54 -0400

    fixed lack of /dev/console in initrd

--- initrd/build.sh
+++ initrd/build.sh
@@ -4,6 +4,6 @@
 
 mkdir -p staging_dir temp
 cp -a files/* staging_dir/
-( cd staging_dir/dev; sudo MAKEDEV std hda hdb hdc hdd )
+( cd staging_dir/dev; sudo MAKEDEV std hda hdb hdc hdd console )
 
 mksquashfs staging_dir initrd -all-root -noappend
