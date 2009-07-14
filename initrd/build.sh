d3b038b2ae72397e10aa2098834a686c352d0069
Andrew Armenia <armena@andrewsdesktop.inside.asquaredlabs.com>
2009-07-14 17:36:07 -0400

    added initrd build scripts

---
+++ initrd/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Build NTP plugin
+
+mkdir -p staging_dir temp
+cp -a files/* staging_dir/
+( cd staging_dir/dev; sudo MAKEDEV std hda hdb hdc hdd )
+
+mksquashfs staging_dir initrd -all-root -noappend
