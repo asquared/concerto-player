91de9cbdaf5b6d96a6fe4ffdd76f06140e9c9ee9
Andrew Armenia <armena@andrewsdesktop.inside.asquaredlabs.com>
2009-07-18 00:42:40 -0400

    added etc/defaults.pl to concerto-scripts

---
+++ concerto-scripts/files/etc/defaults.pl
@@ -0,0 +1,5 @@
+$ENV{'CONCERTO_INSTALL'} = 'http://concerto.rpi.edu/';
+$ENV{'CONFIG_ROOT_URL'} = 'http://concerto.rpi.edu/hardware/';
+$ENV{'LOG_DEST'} = 'http://senatedev.union.rpi.edu/failure.php';
+$ENV{'EMERG_AUDIO_BASE'} = 'http://senatedev.union.rpi.edu/andrew/hardware/';
+
