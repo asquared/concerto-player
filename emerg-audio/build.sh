9efa054488741ec9769837e1d4f392170ab745fc
Andrew Armenia <armena@andrewsdesktop.inside.asquaredlabs.com>
2009-07-17 21:33:31 -0400

    emergency audio dependency updates

--- emerg-audio/build.sh
+++ emerg-audio/build.sh
@@ -5,7 +5,7 @@
 mkdir -p staging_dir temp
 cp -a files/* staging_dir/
 
-# FIXME: this is an inefficient way of doing this
 ../scripts/stage-packages.pl alsa-tools alsa-utils mpg123
+../scripts/stage-packages.pl libwww-perl libcrypt-openssl-rsa-perl
 
 mksquashfs staging_dir emerg-audio.plugin -all-root -noappend
