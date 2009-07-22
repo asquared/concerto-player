809762cbf23bbdb894f929c7823b18f353591472
Andrew Armenia <armena@andrewsdesktop.inside.asquaredlabs.com>
2009-07-22 18:44:41 -0400

    initial livecd work

---
+++ livecd/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir -p staging_dir/
+
+# you need to put a kernel in files/kernel 
+# (and its module plugin in files/modules-x.y.zz.plugin) 
+# and isolinux in files/isolinux to make this happy
+
+# copy the external files
+cp -a files/* staging_dir/
+
+# copy the main system .squashfs file
+cp ../base-system/concerto.squashfs staging_dir/
+
+# copy plugins
+for plugin in \
+    blank-cursor-theme concerto-configurator concerto-scripts \
+    firefox-3.5 ntpdate unionfs-workaround
+do
+    cp ../$plugin/*.plugin staging_dir/
+done
+
+# copy initrd
+cp ../initrd/initrd staging_dir/
+
+# and make the ISO...
+mkisofs -o concerto.iso -b isolinux/isolinux.bin -c isolinux/boot.cat \
+    -no-emul-boot -boot-load-size 4 -boot-info-table -r -J staging_dir/
