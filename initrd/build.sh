bd95cff68fa46bf37a809f165798c3ed90e5b35f
Andrew Armenia <armena@andrewsdesktop.inside.asquaredlabs.com>
2009-08-07 22:09:28 -0400

    initrd fixes: use SCSI CD devices, and check to make sure we're really looking at Concerto boot media

--- initrd/build.sh
+++ initrd/build.sh
@@ -4,6 +4,6 @@
 
 mkdir -p staging_dir temp
 cp -a files/* staging_dir/
-( cd staging_dir/dev; sudo ./MAKEDEV std hda hdb hdc hdd sda sdb sdc sdd console )
+( cd staging_dir/dev; sudo ./MAKEDEV std hda hdb hdc hdd sda sdb sdc sdd console scd-all )
 
 mksquashfs staging_dir initrd -all-root -noappend
